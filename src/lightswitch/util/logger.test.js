0bdd04c946164737482011f2fc02e6de8aaca1c0
Kris Witalewski <krzysztofwitalewski@workfront.com>
2018-10-09 09:36:18 +0200

    add logger test for actual logging

--- src/lightswitch/util/logger.test.js
+++ src/lightswitch/util/logger.test.js
@@ -1,10 +1,28 @@
+const sinon = require("sinon");
 const logger = require("./logger");
 
+const MSG = "Unit tests running - testing logger";
+
 describe("logger", () => {
-    test("exposes all methods in use", () => {
-        expect(typeof(logger.error)).toEqual("function");
-        expect(typeof(logger.warn)).toEqual("function");
-        expect(typeof(logger.info)).toEqual("function");
-        expect(typeof(logger.debug)).toEqual("function");
-   });
- });
+  let stub;
+
+  beforeAll(() => {
+    stub = sinon.stub(console._stdout, "write");
+  });
+
+  test("exposes all methods in use", () => {
+    expect(typeof logger.error).toEqual("function");
+    expect(typeof logger.warn).toEqual("function");
+    expect(typeof logger.info).toEqual("function");
+    expect(typeof logger.debug).toEqual("function");
+  });
+
+  test("prints message with appropriate format", () => {
+    logger.info(MSG);
+    expect(stub.called).toEqual(true);
+  });
+
+  afterAll(() => {
+      stub.restore();
+  })
+});
